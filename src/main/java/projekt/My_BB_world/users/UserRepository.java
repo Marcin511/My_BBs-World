a33fe337f7db83f9932d24a6cdfc1856066eb434
Marcin <szpak111@gmail.com>
2023-04-19 19:45:36 +0200

    create create userRepository

---
+++ src/main/java/projekt/My_BB_world/users/UserRepository.java
@@ -0,0 +1,12 @@
+package projekt.My_BB_world.users;
+
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, Id> {
+    Optional<User> findUserById(Long id);
+}
