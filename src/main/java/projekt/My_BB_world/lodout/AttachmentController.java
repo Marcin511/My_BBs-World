94f249de89374a721afe95b95689bb1309fd2dd5
Marcin <szpak111@gmail.com>
2023-04-25 21:01:41 +0200

    create methods in AttachmentController minor fix

--- src/main/java/projekt/My_BB_world/lodout/AttachmentController.java
+++ src/main/java/projekt/My_BB_world/lodout/AttachmentController.java
@@ -1,6 +1,5 @@
 package projekt.My_BB_world.lodout;
 
-import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
