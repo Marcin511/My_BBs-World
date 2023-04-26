7cfac4f4076daf4e4385a228b7eb3bf883984fb3
Marcin <szpak111@gmail.com>
2023-04-26 17:42:24 +0200

    create methods in loadout service

--- src/main/java/projekt/My_BB_world/lodout/LoadoutService.java
+++ src/main/java/projekt/My_BB_world/lodout/LoadoutService.java
@@ -1,7 +1,33 @@
 package projekt.My_BB_world.lodout;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class LoadoutService {
+    private final LoadoutRepository loadoutRepository;
+
+    public LoadoutService(LoadoutRepository loadoutRepository) {
+        this.loadoutRepository = loadoutRepository;
+    }
+    Loadout addLoadout(Loadout loadout){
+       return loadoutRepository.save(loadout);
+    }
+    List<Loadout> showLoadout(){
+        return loadoutRepository.findAll();
+    }
+    Loadout getLoadout(Long loadoutId){
+        return loadoutRepository.findLoadoutById(loadoutId)
+                .orElseThrow(()-> new NoSuchElementException("loadout not exist"));
+    }
+    Loadout updateLoadout(Long loadoutId,@RequestBody Loadout loadoutToUpdate){
+        return loadoutRepository.findLoadoutById(loadoutId)
+                .map(loadout -> {loadout.setType(loadoutToUpdate.getType());
+                loadout.setFps(loadoutToUpdate.getFps());
+                return loadoutRepository.save(loadout);})
+                .orElseThrow(()->new NoSuchElementException("loadout not exist"));
+    }
 }
